79f32020cb5c3458f7492f3bac2cd1be6120a2ec
keit0728 <keit0728@gmail.com>
2020-05-30 22:56:27 +0900

    New Project

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by 長瀬佳史郎 on 2020/05/30.
+//  Copyright © 2020 keit0728. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
